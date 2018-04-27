23b0cbf0e05701fa2fc1d767ffcd481aad52496a
Benjie <benjiecodes@gmail.com>
2018-04-27 15:49:26 -0400

    added instructions on how to enable unicorn overload need to fix logic

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 const inputCode = [];
 const konamiCode =
-  "MetaArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbabaEnter";
+  "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbabaEnter";
 
 window.addEventListener("keyup", e => {
   inputCode.push(e.key);
-  console.log(e.key);
-
   inputCode.splice(
-    -konamiCode.length - 1,
+    -konamiCode.length - 1 + 4,
     inputCode.length - konamiCode.length
   ); // splice method starts from the end of the array to grab the exact length
-  console.log(inputCode.join(""));
-
   if (inputCode.join("").includes(konamiCode)) {
-    console.log("hooray");
+    alert("EASTER EGG ENGAGED! TIME TO EXPERIENCE UNICORN OVERLOAD");
+    cornify_add();
   }
   console.log(inputCode);
 });

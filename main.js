622b4791d2d5279d33751a493b9e64132028f058
Benjie <benjiecodes@gmail.com>
2018-04-27 15:33:59 -0400

    added if statement to log hooray when input matches konami code

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const inputCode = [];
 const konamiCode =
-  "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowLeftArrowRightArrowRightbabaEnter";
+  "MetaArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightbabaEnter";
 
 window.addEventListener("keyup", e => {
   inputCode.push(e.key);
+  console.log(e.key);
+
   inputCode.splice(
     -konamiCode.length - 1,
     inputCode.length - konamiCode.length
-  );
+  ); // splice method starts from the end of the array to grab the exact length
+  console.log(inputCode.join(""));
+
+  if (inputCode.join("").includes(konamiCode)) {
+    console.log("hooray");
+  }
   console.log(inputCode);
 });
